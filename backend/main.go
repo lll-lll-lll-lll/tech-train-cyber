0e1c0aa342775e126de60ed6050a77aed7efe580
0xpeIpeI <hokumaru12@gmail.com>
2022-08-21 23:34:05 +0900

    step2完了

---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type MySql struct {
+	datasource string
+}
+
+func NewMySql(datasource string) *MySql {
+	return &MySql{
+		datasource: datasource,
+	}
+}
+
+func (db *MySql) Open() (*sqlx.DB, error) {
+	dbcon, err := sqlx.Open("mysql", db.datasource)
+	defer dbcon.Close()
+	if err != nil {
+		return nil, fmt.Errorf("failed db init. %s", err)
+	}
+	if err := dbcon.Ping(); err != nil {
+		return nil, err
+	}
+	return dbcon, nil
+}
+
+func main() {
+
+}
