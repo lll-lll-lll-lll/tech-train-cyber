e6270bce3f27c16442ae5e369c3f0c0ff78e2687
0xpeIpeI <hokumaru12@gmail.com>
2022-08-31 01:01:17 +0900

    [add] user api定義

--- backend/api/router.go
+++ backend/api/router.go
@@ -1,9 +1,18 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tech-train-cyber/api/handler"
+)
 
 // InitRouter routerのパスを指定
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+	userapi := r.Group("/api")
+	{
+		userapi.GET("/user/get", handler.GetUser)
+		userapi.POST("/user/create", handler.CreateUser)
+		userapi.POST("/user/update", handler.UpdateUser)
+	}
 	return r
 }
