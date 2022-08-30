ed86ea64f2b09cccf05424b3eba2a5c7f6038a22
0xpeIpeI <hokumaru12@gmail.com>
2022-08-30 10:36:00 +0900

    [add] userテーブル作成

--- db/initdb.d/user.sql
+++ db/initdb.d/user.sql
@@ -1,12 +1,47 @@
-drop schema if exists users;
-create schema users;
-USE users;
-
+-- Use SQL Formatter Extension
+-- drop schema if exists cyberdb;
+-- create schema cyberdb;
 drop table if exists user;
 
-create table user (
-    id int not null auto_increment primary key
-    name varchar(255) not null
+drop table if exists user_update;
+
+-- リソース
+CREATE TABLE `user` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `token` varchar(255) NOT NULL
 );
 
-insert int user (name) values ("shunpei")
+-- イベント
+CREATE TABLE `update_user` (
+    `updated_id` int PRIMARY KEY NOT NULL,
+    `user_id` int,
+    `user_name` varchar(255),
+    `updated_at` datetime NOT NULL DEFAULT (now())
+);
+
+-- イベント
+CREATE TABLE `update_auth_token` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `user_id` int,
+    `old_auth_token` varchar(255) NOT NULL,
+    `new_auth_token` varchar(255) NOT NULL,
+    `updated_at` datetime NOT NULL DEFAULT (now())
+);
+
+-- 外部キー追加
+ALTER TABLE
+    `update_user`
+ADD
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE
+    `update_auth_token`
+ADD
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+-- 適当に値を追加
+insert into
+    user (name, token)
+values
+    ("shunpei", "token_shunpei")
