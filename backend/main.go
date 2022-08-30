9e51be93bd90ef707517333b6ae1709aefc30ec7
0xpeIpeI <hokumaru12@gmail.com>
2022-08-31 00:47:42 +0900

    [modify] serverの初期化を移動

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/tech-train-cyber/db"
+	"github.com/tech-train-cyber/api"
 )
 
 func main() {
-	d := db.DBConfig{
-		User:     os.Getenv("MYSQL_USER"),
-		Password: os.Getenv("MYSQL_PASSWORD"),
-		Host:     os.Getenv("MYSQL_HOST"),
-		Port:     os.Getenv("MYSQL_PORT"),
-		DBName:   os.Getenv("MYSQL_DATABASE"),
-	}.String()
-	md := db.NewMySql(d)
-	db, err := md.Open()
-	defer db.Close()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
+	server := api.NewServer()
+	server.Run(80)
 }
