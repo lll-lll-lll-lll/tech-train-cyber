187d4e0e99bdfb3582dc361de76a48dc22e3a18b
0xpeIpeI <hokumaru12@gmail.com>
2022-08-31 01:01:25 +0900

    [add] user handler追加

--- backend/api/handler/user.go
+++ backend/api/handler/user.go
@@ -1,7 +1,15 @@
 package handler
 
+import "github.com/gin-gonic/gin"
+
 type UserHandler struct{}
 
 func NewUserHandler() *UserHandler {
 	return &UserHandler{}
 }
+
+func GetUser(c *gin.Context) {}
+
+func CreateUser(c *gin.Context) {}
+
+func UpdateUser(c *gin.Context) {}
