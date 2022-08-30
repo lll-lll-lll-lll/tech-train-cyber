8120966c6c6476e5d162f166584e27ab305933e4
0xpeIpeI <hokumaru12@gmail.com>
2022-08-31 00:50:01 +0900

    [modify] serverの初期化メソッドを追加

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/tech-train-cyber/api"
 )
 
 func main() {
 	server := api.NewServer()
+	err := server.InitServer()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	server.Run(80)
 }
